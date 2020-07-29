a7356153e369b305e76ebcd0c1f5ab6910ebddc1
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2020-07-29 16:36:39 -0400

    Added side bar and started working on adding new notes to it.

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -3,8 +3,9 @@ import ReactQuill from 'react-quill';
 import debounce from '../helper';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem'; 
 import styles from './styles';
-import { Divider, Button} from '@material-ui/core';
+import { Divider, Button, List} from '@material-ui/core';
 import SidebaritemComponent from '../sidebaritem/sidebaritem';
 
 class SidebarComponent extends React.Component {
@@ -19,24 +20,52 @@ class SidebarComponent extends React.Component {
     }
     render() {
         const {notes, classes, selectedNoteIndex} = this.props;
-        return(
-            <div className = {classes.sidebarContainer}>
-                    <Button
-                    onClick = {this.newNoteBtnClick}
-                    className = {classes.newNoteBtn}>New Note</Button>
-                    {
-                        this.state.addingNote ? 
-                        <div>
-                            <input type = 'text'
-                            className = {classes.newNoteInput}
-                            placeholder = 'Enter note title'
-                            onKeyUp = {(e) => this.updateTitle(e.target.value)}>
-                            </input>
-                        </div>:
-                        null
-                    }
-                </div>
-        ); 
+        if(notes)
+        {
+            return(
+                <div className = {classes.sidebarContainer}>
+                        <Button
+                        onClick = {this.newNoteBtnClick}
+                        className = {classes.newNoteBtn}>{this.state.addingNote? 'Cancel': 'New Note'}</Button>
+                        {
+                            this.state.addingNote ? 
+                            <div>
+                                <input type = 'text'
+                                className = {classes.newNoteInput}
+                                placeholder = 'Enter note title'
+                                onKeyUp = {(e) => this.updateTitle(e.target.value)}>
+                                </input>
+                                <Button className = {classes.newNoteSubmitBtn}
+                                onClick = {this.newNote}
+                                >Submit Note</Button>
+                            </div>:
+    
+                            null
+                        }
+                        <List>
+                            {
+                                notes.map((_note, _index) => {
+                                    return(
+                                        <div key = {_index}>
+                                            <SidebaritemComponent
+                                             _note = {_note}
+                                             _index = {_index}
+                                             selectedNoteIndex = {selectedNoteIndex}
+                                             selectNote = {this.selectNote}
+                                             deleteNote = {this.deleteNote}>
+                                             </SidebaritemComponent>
+                                             <Divider></Divider>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </List>
+                    </div>
+            ); 
+        }
+        else {
+            return (<div>Add a note!</div>);        
+        }
     }
     newNoteBtnClick  = () => {
         console.log('new btn clicked'); 
@@ -44,6 +73,16 @@ class SidebarComponent extends React.Component {
     }
     updateTitle = (txt) => {
         console.log('Here it is: ', txt); 
+        this.setState({title: txt});
+    }
+    newNote = () => {
+        console.log(this.state);
+    }
+    selectNote = () => {
+
+    }
+    deleteNote = () => {
+
     }
 }
 export default withStyles(styles)(SidebarComponent);
--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -3,7 +3,12 @@ import ReactQuill from 'react-quill';
 import debounce from '../helper';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem'; 
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { removeHTMLTags } from '../helper'; 
 import styles from './styles';
+import { Divider, Button} from '@material-ui/core';
 
 class SidebaritemComponent extends React.Component {
     constructor()
@@ -11,8 +16,32 @@ class SidebaritemComponent extends React.Component {
         super();
 
     }
-    render() {
-    return(<div>Hello from the sidebar item</div>);
-}
+    render(){
+        const{_index, _note, classes, selectedNoteIndex} = this.props;
+        return(
+            <div key = {_index}>
+                <ListItem
+                    className = {classes.ListItem}
+                    selected = {selectedNoteIndex === _index}
+                    alignItems  = 'flex start'>
+                    <div 
+                        className = {classes.textSelection}
+                        onClick = {() => this.selectNote(_note, _index)}>
+                        <ListItemText
+                            primary = {_note.title}
+                            secondary = {removeHTMLTags(_note.body.substring(0, 30))+'...'}
+                            >
+                        </ListItemText>
+                    </div>
+                    <DeleteIcon
+                        onClick = {() => this.deleteNote(_note)}
+                        className = {classes.deleteIcon}
+                        >
+
+                    </DeleteIcon>
+                </ListItem>
+            </div>
+        );
+    }
 }
 export default withStyles(styles)(SidebaritemComponent);
